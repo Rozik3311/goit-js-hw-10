9607c94023ce17451c52594e275ba45d8e1c9444
Rozik3311 <115663456+Rozik3311@users.noreply.github.com>
2023-06-06 19:23:49 -0400

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-import SlimSelect from 'slim-select'
+// import SlimSelect from 'slim-select'
 import Notiflix from 'notiflix';
 
-new SlimSelect({
-  select: '#selectElement'
-})
+// new SlimSelect({
+//   select: 'select.breed-select'
+// })
+
+Notiflix.Notify.success('You are welcome');
 
 // Выполнение HTTP-запроса за коллекцией пород
 fetchBreeds()
@@ -18,36 +20,43 @@ fetchBreeds()
       breedSelect.appendChild(option);
     });
   })
-  .catch(error => {
-    console.error('Ошибка при загрузке коллекции пород:', error);
+  .catch((error) => {
+    // Проверяем тип ошибки
+    if (error instanceof TypeError) {
+      // Выводим текст об ошибке в элементе параграфа
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.textContent = 'Oops! Something went wrong! Try reloading the page!';
+    }
+
   });
 
-// Обработчик события выбора опции в селекте
-document.querySelector('select.breed-select').addEventListener('change', event => {
-  const breedId = event.target.value;
+  document.querySelector('.breed-select').addEventListener('change', event => {
+    const breedId = event.target.value;
   
-  // Выполнение HTTP-запроса за данными о коте
-  fetchCatByBreed(breedId)
-    .then(catData => {
-      // Обновление интерфейса с информацией о коте
-      const catInfo = document.querySelector('div.cat-info');
-      catInfo.innerHTML = ''; // Очищаем содержимое блока
-      
-      if (catData.length > 0) {
-        const cat = catData[0];
-        const image = document.createElement('img');
-        image.src = cat.url;
-        catInfo.innerHTML = `
-          <p><strong>Название породы:</strong> ${cat.breeds[0].name}</p>
-          <p><strong>Описание:</strong> ${cat.breeds[0].description}</p>
-          <p><strong>Темперамент:</strong> ${cat.breeds[0].temperament}</p>
-        `;
-        catInfo.appendChild(image);
-      }
-    })
-    .catch(error => {
-      console.error('Ошибка при загрузке данных о коте:', error);
-    });
-});
+    // Выполнение HTTP-запроса за данными о коте
+    fetchCatByBreed(breedId)
+      .then(catData => {
+        // Обновление интерфейса с информацией о коте
+        const catInfo = document.querySelector('div.cat-info');
+        catInfo.innerHTML = ''; // Очищаем содержимое блока
+  
+        if (catData) {
+          const cat = catData[0];
+          const image = document.createElement('img');
+          image.src = cat.url;
+          catInfo.innerHTML = `
+            <p><strong>Название породы:</strong> ${cat.breeds[0].name}</p>
+            <p><strong>Описание:</strong> ${cat.breeds[0].description}</p>
+            <p><strong>Темперамент:</strong> ${cat.breeds[0].temperament}</p>
+          `;
+          catInfo.appendChild(image);
+        }
+      })
+      .catch((error) => {
+        const errorMessage = document.querySelector('.error');
+        errorMessage.classList.add('active');
+      });
+  });
+
 
 
